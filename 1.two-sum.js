11f8bc69da7b3f71db2eaf219d20672c7b24b65f
Kathy Ye <kathyye15@gmail.com>
2023-06-14 23:45:14 -0700

    cleaned up code

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -11,18 +11,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  //object of seen (value, index)
   let seen = {};
-  //iterate over array
   for (let i = 0; i < nums.length; i++) {
       let currNum = nums[i];
       let wantedNum = target - currNum;
-      //check if wantedNum has been seen
-      if (seen[wantedNum] || seen[wantedNum] === 0) {
-          //return the answer if yes
+      if (wantedNum in seen) {
           return [i, seen[wantedNum]];
       } else {
-          //otherwise, add currNum to seen 
           seen[currNum] = i;
       }
   }
