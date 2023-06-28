21d7a0bb91e2087eca4f8c5207181e7c149e73ef
Kathy Ye <kathyye15@gmail.com>
2023-06-27 21:52:28 -0700

    wip binary heap (priority queue) pseudocode

--- 373.find-k-pairs-with-smallest-sums.js
+++ 373.find-k-pairs-with-smallest-sums.js
@@ -11,37 +11,43 @@
  * @param {number} k
  * @return {number[][]}
  */
-var kSmallestPairs = function(nums1, nums2, k) {
-  //create result array
-  let result = [];
-  let index1 = 0;
-  let index2 = 0;
-  //while length less than k and length less than possible combinations
-  while (result.length < k && result.length < nums1.length * nums2.length) {
-      if (nums2[index2] === undefined) {
-          index2--;
-          index1++;
-          continue;
-      }
-      if (nums1[index1] === undefined) {
-          index1--;
-          index2++;
-          continue;
-      } 
-      if (nums1[index1] < nums2[index2]) {
-          for (let i = 0; i < nums2.length && result.length < k; i++) {
-              result.push([nums1[index1], nums2[i]]);
-          }
-          index2++;
-      } else {
-          for (let j = 0; j < nums1.length && result.length < k; j++) {
-              result.push([nums1[j], nums2[index2]]);
-          }
-          index1++;
-      }
+class MinPriorityQueue {
+  constructor() {
+      this.values = [];
   }
-  //return result
-  return result;
+  size() {
+      return this.values.length;
+  }
+  //use sum as priority num
+  enqueue(indexes, sum) {
+      //create element object
+      //push element into this.values;
+      //call bubbleUp;
+  }
+  dequeue() {
+      //pop the element
+      //call sinkDown
+  }
+  bubbleUp() {
+
+  }
+  sinkDown() {
+      
+  }
+}
+var kSmallestPairs = function(nums1, nums2, k) {
+//create a new MinPriorityQueue
+//create result array
+
+//enqueue all pair combos with num1 values & num2 index 0 val
+
+//while result length is less than k and pq size is > 0
+  //dequeue indexes (ind1, ind2) 
+  //push corresponding values using indexes into result array
+  //enqueue next possible choice (ind1 + 1, ind2, sum)
+
+//return result array
+
 };
 // @lc code=end
 
