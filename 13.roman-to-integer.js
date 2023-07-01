433ffff2808f26dd1f514c8f5142fe3738dd1632
Kathy Ye <kathyye15@gmail.com>
2023-06-30 23:56:12 -0700

    linear time and constant space

--- 13.roman-to-integer.js
+++ 13.roman-to-integer.js
@@ -22,19 +22,22 @@ var romanToInt = function(s) {
   }
   for (let i = 0; i < s.length; i++) {
       let j = i;
+      let k = i;
       let symbol = s[i];
-      if (symbol == 'I' && (s[j++] == 'V' || s[j++] == 'X')) {
+      if (symbol == 'I' && (s[++j] == 'V' || s[++k] == 'X')) {
           num -=1;
-      } else if (symbol == 'X' && (s[j++] == 'L' || s[j++] == 'C')) {
+      } else if (symbol == 'X' && (s[++j] == 'L' || s[++k] == 'C')) {
           num -=10;
-      } else if (symbol == 'C' && (s[j++] == 'D' || s[j++] == 'M')) {
+      } else if (symbol == 'C' && (s[++j] == 'D' || s[++k] == 'M')) {
           num -=100;
       } else {
           num += lookUp[symbol];
       }
-
   }
   return num;
 };
+
+//time: o(n)
+//space: o(1)
 // @lc code=end
 
