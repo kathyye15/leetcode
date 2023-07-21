b15275d0399ebcfa3fbc823ac786042fe0a62713
Kathy Ye <kathyye15@gmail.com>
2023-07-20 23:52:24 -0700

    achieve same constant space and time complexity with less lines of code

--- 1768.merge-strings-alternately.js
+++ 1768.merge-strings-alternately.js
@@ -11,25 +11,16 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-  let merged = [];
-  let leftoverWord;
-  if (word1.length > word2.length) {
-      leftoverWord = word1.slice(word2.length);
-  } else if (word2.length > word1.length) {
-      leftoverWord = word2.slice(word1.length);
-  }
-  let min = Math.min(word1.length, word2.length);
-  for (let i=0; i< min; i++) {
-      let letter = word1[i];
-      merged[i*2]=letter;
-  }
-  for (let j=0; j< min; j++) {
-      let letter = word2[j];
-      merged[j*2+1]=letter;
-  }
-  merged = merged.join('');
-  if (leftoverWord) {
-      return merged + leftoverWord;
+  let merged = '';
+  let i = 0;
+  while (i < word1.length + word2.length) {
+      if (i < word1.length) {
+          merged+=word1[i];
+      }
+      if (i < word2.length) {
+          merged+=word2[i];
+      }
+      i++;
   }
   return merged;
 };
