5e7ce80014b20dedb227d8558dee1acdfc8c22c7
Kathy Ye <kathyye15@gmail.com>
2023-07-27 20:19:50 -0700

    code cleanup

--- 1071.greatest-common-divisor-of-strings.js
+++ 1071.greatest-common-divisor-of-strings.js
@@ -15,17 +15,11 @@
  //c - input strings are all uppercase, string length 1 to 1000
  //e - 
  var gcdOfStrings = function(str1, str2) {
-  //check if the substrings share a common substring
-  if (str1 + str2 !== str2 + str1) {
-      return '';
-  }
-  //slight optimization, checking minLength to lower iteration
+  if (str1 + str2 !== str2 + str1) return '';
   let minLength = Math.min(str1.length, str2.length);
   for (let l = minLength; l > 0; l--) {
-      //check if str1 and str2 lengths are divisable with length
       if ((str1.length % l === 0) && (str2.length % l === 0)) {
-              //return str1 substring up to that length
-              return str1.substring(0, l);
+          return str1.substring(0, l);
       }
   }
 };
