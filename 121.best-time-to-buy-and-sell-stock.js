64a658c783fc85dddf8d3d04baeb7cf2358457ff
Kathy Ye <kathyye15@gmail.com>
2023-11-14 23:50:07 -0800

    refactor: remove unused logic

--- 121.best-time-to-buy-and-sell-stock.js
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -10,27 +10,27 @@
  * @return {number}
  */
 
- //i - array of nums
- //o - number (max profit between buy low, sell high)
- //c - array length 1 to 10^5, number value 0 to 10^4
- //e -
-var maxProfit = function(prices) {
+//i - array of nums
+//o - number (max profit between buy low, sell high)
+//c - array length 1 to 10^5, number value 0 to 10^4
+//e -
+var maxProfit = function (prices) {
   let profit = 0;
-  let start = 0;
-  let end = 0;
+  let buyIndex = 0;
+  let sellIndex = 0;
   for (let i = 0; i < prices.length; i++) {
-      let price = prices[i];
-      if (price < prices[start]) {
-          start = i;
-          end = Math.max(start, end);
-      }
-      if (price > prices[end]) {
-          end = i;
-      }
-      profit = Math.max(profit, prices[end] - prices[start]);
+    let price = prices[i];
+    if (price < prices[buyIndex]) {
+      buyIndex = i;
+      sellIndex = buyIndex;
+    }
+    if (price > prices[sellIndex]) {
+      sellIndex = i;
+    }
+    profit = Math.max(profit, prices[sellIndex] - prices[buyIndex]);
   }
   return profit;
-}
+};
 // @lc code=end
 //time - O(n)
 //space - O(1)
