7690a85dfccf0b807ce3953c614e1258ccbb0d69
Kathy Ye <kathyye15@gmail.com>
2024-03-05 23:03:50 -0800

    improved space complexity from linear to constant

--- 238.product-of-array-except-self.js
+++ 238.product-of-array-except-self.js
@@ -11,40 +11,26 @@
  */
 /*
 [1,2,3,4]
-
 pre: [1,1,2,6]
 suff: [24,12,4,1]
-
 ans: [24,12,8,6]
- */
+*/
 
-//[1,2,3,4]
-//pre: [1, 1, 2, 6]
-//suff: [ , , ,1]
+/* 
+nums: [1,2,3,4]
+ans: [24,12,8,6]
+suff = 24
+*/
 var productExceptSelf = function (nums) {
-  let pre = [];
-  let suff = [];
-  let ans = [];
-  pre[0] = 1;
-  suff[nums.length - 1] = 1;
+  let ans = new Array(nums.length).fill(1);
   for (let i = 1; i < nums.length; i++) {
-    pre[i] = pre[i - 1] * nums[i - 1];
+    ans[i] = ans[i - 1] * nums[i - 1];
   }
+  let suff = nums[nums.length - 1];
   for (let i = nums.length - 2; i > -1; i--) {
-    suff[i] = suff[i + 1] * nums[i + 1];
-  }
-
-  for (let i = 0; i < nums.length; i++) {
-    ans[i] = pre[i] * suff[i];
+    ans[i] = ans[i] * suff;
+    suff = suff * nums[i];
   }
   return ans;
 };
-
-/* improve space complex to O(1)
-[1,2,3,4]
-  curr = 24;
-  ans = [24,12,8,6]
-
-*/
-
 // @lc code=end
