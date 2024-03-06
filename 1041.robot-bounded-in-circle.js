268fe9c5497b829862a083d1419ab22204eef57f
Kathy Ye <kathyye15@gmail.com>
2024-03-05 22:55:56 -0800

    implement linear time, constant space solution

---
+++ 1041.robot-bounded-in-circle.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1041 lang=javascript
+ *
+ * [1041] Robot Bounded In Circle
+ */
+
+// @lc code=start
+/**
+ * @param {string} instructions
+ * @return {boolean}
+ */
+var isRobotBounded = function(instructions) {
+    
+};
+// @lc code=end
+
