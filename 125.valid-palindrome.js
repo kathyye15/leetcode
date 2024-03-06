f93a74f28504ab930c90de362f3f721d03218114
Kathy Ye <kathyye15@gmail.com>
2024-03-05 22:58:35 -0800

    solve using regex

--- 125.valid-palindrome.js
+++ 125.valid-palindrome.js
@@ -9,8 +9,11 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    
+var isPalindrome = function (s) {
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+  for (let i = 0, j = s.length - 1; j > i; i++, j--) {
+    if (s[i] !== s[j]) return false;
+  }
+  return true;
 };
 // @lc code=end
-
